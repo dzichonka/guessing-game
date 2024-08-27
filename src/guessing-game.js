bfbbac5daafa3dbc021095e5ee057434df2df0ea
Anna Vasilevich <dzichonka@gmail.com>
2024-08-27 12:09:51 +0200

    feat: pass 100 tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.floor((this.min + this.max + 1) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        // Сужаем диапазон сверху
+        this.max = this.currentGuess;
     }
 
     greater() {
-
+        this.min = this.currentGuess;
     }
 }
 
